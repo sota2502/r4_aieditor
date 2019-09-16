47317587c935e99f37fdf490b7e415bfcb3c843d
sota2502 <sota.nakamori@example.com>
2019-09-16 13:36:46 +0900

    Add basic authentication method to prevent unknown user access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+    def basic_auth
+      basic_authentication = Settings.basic_authentication
+      authenticate_or_request_with_http_basic do |user, password|
+        user == basic_authentication.user &&
+          password == basic_authentication.password
+      end
+    end
 end
