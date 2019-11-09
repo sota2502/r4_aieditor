5d20ad594d6590fdf39ec73955c73c1b9fa0461b
sota2502 <sota.nakamori@example.com>
2019-11-09 22:39:43 +0900

    Nullable

--- app/forms/cancel_conditions_form.rb
+++ app/forms/cancel_conditions_form.rb
@@ -20,14 +20,22 @@ class CancelConditionsForm
     def update_cancel_condition
       cancel_conditions = @cancel_conditions_params[:cancel_conditions].to_h.map do |_, param|
         CancelCondition.new(param)
-      end
+      end.select { |c| valid?(c) }
       CancelCondition.import cancel_conditions, on_duplicate_key_update: ATTRIBUTES
     end
 
     def create_cancel_condition
-      @cancel_conditions_params[:new_cancel_conditions].to_a.map do |param|
-        cancel_condition = CancelCondition.create!(param)
+      cancel_conditions = @cancel_conditions_params[:new_cancel_conditions].to_a.map do |param|
+        cancel_condition = CancelCondition.new(param)
+      end.select { |c| valid?(c) }
+
+      cancel_conditions.each do |cancel_condition|
+        cancel_condition.save!
         @parent.add_cancel_condition(cancel_condition)
       end
+    end
+
+    def valid?(cancel_condition)
+      cancel_condition.cancel_type_id&.present?
     end
 end
