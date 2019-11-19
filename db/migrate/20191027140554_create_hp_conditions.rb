be8df0532d42362b9b2ea3c04d6467d6c8d53158
sota2502 <sota.nakamori@example.com>
2019-11-19 23:48:03 +0900

    Fix column type

--- db/migrate/20191027140554_create_hp_conditions.rb
+++ db/migrate/20191027140554_create_hp_conditions.rb
@@ -2,7 +2,7 @@ class CreateHpConditions < ActiveRecord::Migration[6.0]
   def change
     create_table :hp_conditions do |t|
       t.references :exaction, null: false, foreign_key: true
-      t.unsigned_integer :value, null: false
+      t.unsigned_float :value, null: false
 
       t.timestamps
     end
