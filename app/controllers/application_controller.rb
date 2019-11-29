15bd2fbe519827445ce24b0e34373aa498c2df52
sota2502 <sota.nakamori@example.com>
2019-11-29 21:21:55 +0900

    Add sidemenu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :set_cache_headers
+  before_action :set_sidebar
 
   private
 
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
       response.headers["Cache-Control"] = "no-cache, no-store"
       response.headers["Pragma"] = "no-cache"
       response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
+
+    def set_sidebar
+      @projects_for_sidebar = Project.includes(:action_state, :action_chains, :exaction).all
     end
 end
